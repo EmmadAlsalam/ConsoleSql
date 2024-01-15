3f3b32f6bd0164c5297237866f39c85104f7ca4a
EmmadAlsalam <143703288+EmmadAlsalam@users.noreply.github.com>
2024-01-15 19:02:40 +0100

    Har lagt i Git

--- ConsoleSql/Repositories/CustomerRepository.cs
+++ ConsoleSql/Repositories/CustomerRepository.cs
@@ -11,18 +11,7 @@ internal class CustomerRepository(string connectionString)
     public int Create(CustomersEntity entity)
     {
         using var conn = new SqlConnection(_connectionString);
-        var result = conn.ExecuteScalar<int>(@"
-    IF NOT EXISTS (SELECT 1 FROM Customars WHERE Email = @Email)
-    BEGIN
-        INSERT INTO Customars (FirstName, LastName, Email, PhoneNumder, AddressId)
-        OUTPUT INSERTED.Id
-        VALUES (@FirstName, @LastName, @Email, @PhoneNumder, @AddressId)
-    END
-    ELSE
-    BEGIN
-        SELECT Id FROM Customars WHERE Email = @Email
-    END",
-    entity);
+        var result = conn.ExecuteScalar<int>(" IF NOT EXISTS (SELECT 1 FROM Customars WHERE Email=@Email)INSERT INTO Customars OUTPUT INSERTED . Id VALUES (@FirstName,@LastName,@Email,@PhoneNumder,@AddressId)", entity);
 
 
         return result;
